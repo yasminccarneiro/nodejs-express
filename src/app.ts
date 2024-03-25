338954c67c18e2f836a2b32c36e9b3a01fe9d36a
Yasmin Carneiro <yasmin.carneiro@vrsoft.com.br>
2024-03-25 20:07:52 -0300

    18/03

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,27 @@
+// console.log("Hello, CodeLab!!!")
 
 import express from 'express'
+import { Client } from 'pg'
 
-const app = express()
-const port = 3000
+async function main() {
 
+  const app = express()
+  const port = 3000
 
-app.get('/',(req,res)=>{
-    res.send('Hello World e ai?')
-})
+  const client = new Client()
+  await client.connect()
+  
+  const res = await client.query('SELECT $1::text as message', ['DB Connection is OK!'])
+  console.log(res.rows[0].message)
+  await client.end()
 
-app.listen(port,()=>{
+  app.get('/', (req, res) => {
+    res.send('Hello, CodeLab...')
+  })
+
+  app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+  })
+}
 
-console.log("Hello, CodeLab!");
+main()
